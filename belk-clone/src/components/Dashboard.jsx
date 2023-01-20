7f8e0615a9c183c08aeb9305c91dce9e21894e69
Sourav Lahiri <cesouravlahiri4098@gmail.com>
2023-01-20 20:05:31 +0530

    day-3-submission

---
+++ belk-clone/src/components/Dashboard.jsx
@@ -0,0 +1,298 @@
+import React from "react";
+import {
+  Box,
+  Image,
+  Center,
+  Flex,
+  Text,
+  Heading,
+  Stack,
+  Button,
+  Container,
+} from "@chakra-ui/react";
+
+const Dashboard = () => {
+  return (
+    <>
+      {/* // Dashboard */}
+      <Center>
+        <Box position="absolute" top="150px">
+          <Flex direction="column" gap="5">
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_011623_hp_fh1?$DWP_PHOTO$" />
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_011623_hp_fh2?$DWP_PHOTO$" />
+            </Box>
+            <Flex>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_011623_hp_4c1_carl?$DWP_PHOTO$" />
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_011623_hp_4c2_carl?$DWP_PHOTO$" />
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_011623_hp_4c3_carl?$DWP_PHOTO$" />
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_011623_hp_4c4_carl?$DWP_PHOTO$" />
+            </Flex>
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_fh1?$DWP_PHOTO$" />
+              <Flex>
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_4c_1?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_4c_2?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_4c_3?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_4c_4?$DWP_PHOTO$" />
+              </Flex>
+              <Flex>
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_4c_carl_5?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_4c_carl_6?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_4c_carl_7?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_themed_4c_carl_8?$DWP_PHOTO$" />
+              </Flex>
+            </Box>
+            <Box>
+              <center>
+                <Text>
+                  *Select Styles. While quantities last. Not Coupon eligible
+                </Text>
+              </center>
+            </Box>
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk50_2023_trendedit_fh?$DWP_PHOTO$" />
+            </Box>
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_011723_hp_fh1?$DWP_PHOTO$" />
+              <Flex>
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_011723_hp_2c1?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_011723_hp_2c2?$DWP_PHOTO$" />
+              </Flex>
+            </Box>
+            <Box>
+              <Flex>
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_general_blkred_trans_3c_1?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_general_blkred_trans_3c_2?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_ws_general_blkred_trans_3c_3?$DWP_PHOTO$" />
+              </Flex>
+            </Box>
+            <Box>
+              <center>
+                <Text>
+                  *Select Styles. While quantities last. Not Coupon eligible
+                </Text>
+              </center>
+            </Box>
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk50_2023_online_exclusive_hp_fh_1?$DWP_PHOTO$" />
+            </Box>
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/mrf_wk51_2023_loyalty_conns_fh1?$DWP_PHOTO$" />
+            </Box>
+            <Box>
+              <center>
+                <Text>*Online at belkCart.com & Select Stores Only</Text>
+              </center>
+            </Box>
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_2023_brplus_loyalty_fh2?$DWP_PHOTO$" />
+            </Box>
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk51_2022_coupons_hp_fh?$DWP_PHOTO$" />
+            </Box>
+            <Box>
+              <Flex>
+                <Image src="https://belk.scene7.com/is/image/Belk/wk51_011623_hp_fh4?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk50_010923_hp_2c3?$DWP_PHOTO$" />
+              </Flex>
+            </Box>
+            <Box>
+              <Image src="https://belk.scene7.com/is/image/Belk/wk49_010223_hp_fh4?$DWP_PHOTO$" />
+            </Box>
+            <Box>
+              <Flex justifyContent="space-evenly">
+                <Image src="https://belk.scene7.com/is/image/Belk/wk48_122622_hp_6c_3?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk48_122622_hp_6c_2?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk49_010223_hp_6c_6?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk48_122622_hp_6c_4?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk48_122622_hp_6c_5?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk48_122622_hp_6c_1?$DWP_PHOTO$" />
+              </Flex>
+            </Box>
+            <Box>
+              <Flex>
+                <Image src="https://belk.scene7.com/is/image/Belk/wk33_2022_brplus_combo_nonch_2c1?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk33_2022_brplus_combo_nonch_2c2?$DWP_PHOTO$" />
+              </Flex>
+            </Box>
+            <Box>
+              <Flex>
+                <Image src="https://belk.scene7.com/is/image/Belk/wk03_2022_services_4c_1?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk03_2022_services_4c_2?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk03_2022_services_4c_3?$DWP_PHOTO$" />
+                <Image src="https://belk.scene7.com/is/image/Belk/wk03_2022_services_4c_4?$DWP_PHOTO$" />
+              </Flex>
+            </Box>
+            <Box>
+              <Heading fontSize="xl">Belk - Shop & Save Today</Heading>
+            </Box>
+            <hr style={{ border: "1px solid black" }} />
+            <Box>
+              <Container maxW="6xl">
+              <Text>
+              From luggage sets to crossbody bags and fine jewelry, you’ll find what you’re shopping for (and so much more!) on belk.com. Shop designer handbags from many brands including Brahmin and Michael Kors, skincare and makeup from your favorites like Estée Lauder and Clinique or men’s suits and sport coats from stylish brands like Tommy Hilfiger. Get chic in a variety of shoes for women, get active in Nike® or get outdoors in Columbia and Sperry®. Update your home with bedspreads, comforter sets and new cookware. Create picture-perfect looks with our large selection of kids’ and baby clothes. Treat yourself to something new with our wide variety of purses, jewelry and accessories. If you’re searching for luxury gifts, shop our selection of designer watches and Le Vian® jewelry. No matter your style or your lifestyle, you’ll find something you love at Belk. Happy shopping!
+              </Text>
+              </Container>
+            </Box>
+            <Box>
+              <Heading fontSize="xl">Trending Categories</Heading>
+            </Box>
+            <hr style={{ border: "1px solid black" }} />
+            <Box>
+              <Stack direction="row" spacing={4} align="center">
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Men's Watches
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Women's Shoes
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Perfumes
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Men's Shoes
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Women's Boots
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Luggage Sets
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Women's Watches
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Girl's Dresses
+                </Button>
+              </Stack>
+            </Box>
+
+            <Box>
+              <Heading fontSize="xl">Trending Brands</Heading>
+            </Box>
+            <hr style={{ border: "1px solid black" }} />
+            <Box>
+              <Stack direction="row" spacing={4} align="center">
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Nine West
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Brahmin
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Micheal
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Levi's
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Sperry
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Luggage Sets
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Ugg
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Le Vian
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Clinique
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Columbia
+                </Button>
+                <Button
+                  colorScheme="black"
+                  variant="outline"
+                  _hover={{ background: "teal", color: "white" }}
+                >
+                  Free People
+                </Button>
+              </Stack>
+            </Box>
+
+            {/* Footer */}
+            <hr style={{ border: "1px solid black" }} />
+          </Flex>
+        </Box>
+      </Center>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ belk-clone/src/components/NavUpperMenu.jsx
@@ -0,0 +1,348 @@
+import React from "react";
+import {
+  Heading,
+  Box,
+  Flex,
+  Input,
+  Button,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
+import { BsPlusSquare } from "react-icons/bs";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { BsBag } from "react-icons/bs";
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { BiStoreAlt } from "react-icons/bi";
+
+const NavUpperMenu = () => {
+  return (
+    <Box position="fixed" zIndex="1" w="full">
+      <Box bgColor="whitesmoke" p="15px">
+        <Flex justifyContent="space-around">
+          <Box>
+            <Heading as="h2" fontSize="30px">
+              BelkCart
+            </Heading>
+          </Box>
+          <Box>
+            <Input
+              placeholder="What can we help you find?"
+              w="700px"
+              p="10px"
+              border="1px solid black"
+            />
+          </Box>
+          <Box>
+            <UnorderedList listStyleType="none">
+              <Flex gap="18px">
+                <ListItem>
+                  <BsPlusSquare />
+                  <Button colorScheme="black" variant="link">
+                    Belk Reward+
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <MdOutlineLocalOffer />
+                  <Button colorScheme="black" variant="link">
+                    Coupons
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <MdOutlineAccountCircle />
+                  <Button colorScheme="black" variant="link">
+                    My Account
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <BsBag />
+                  <Button colorScheme="black" variant="link">
+                    Bag
+                  </Button>
+                </ListItem>
+              </Flex>
+            </UnorderedList>
+          </Box>
+        </Flex>
+      </Box>
+      <Box>
+        <Flex>
+          <Box>
+            <Menu>
+              <MenuButton
+                bgColor="whitesmoke"
+                fontFamily="serif"
+                p="10px 10px 10px 30px"
+              >
+                Gifts
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Women
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Men
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Kids
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Shoes
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Handbags
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Jewelry
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Beauty
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Home
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Bed & Bath
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+              >
+                Brands
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Beauty Gifts</MenuItem>
+                <MenuItem>Cozy Shop</MenuItem>
+                <MenuItem>Designer Handbags</MenuItem>
+                <MenuItem>Eco-Friendly</MenuItem>
+                <MenuItem>Fine Jwelery</MenuItem>
+                <MenuItem>For the Trend Setter</MenuItem>
+                <MenuItem>Gift Cards</MenuItem>
+                <MenuItem>Gifts with a purpose</MenuItem>
+                <MenuItem>Holiday Decor</MenuItem>
+                <MenuItem>Holiday Pajamas</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu>
+              <MenuButton
+                p="10px 10px 10px 30px"
+                bgColor="whitesmoke"
+                fontFamily="serif"
+                color="red.500"
+              >
+                Clearence
+              </MenuButton>
+              <MenuList bgColor="whitesmoke">
+                <MenuItem>Women's Clearence</MenuItem>
+                <MenuItem>Kids & Boys Clearence</MenuItem>
+                <MenuItem>Handbags and Accessories Clearence</MenuItem>
+                <MenuItem>Beauty Clearence</MenuItem>
+                <MenuItem>Men's Clearence</MenuItem>
+                <MenuItem>Shoes Clearence</MenuItem>
+                <MenuItem>Bed & Bath's Clearence</MenuItem>
+                <MenuItem>Jwelery Clearence</MenuItem>
+                <MenuItem>Juniors Clearence</MenuItem>
+                <MenuItem>Holiday Pajamas Clearence</MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+          <Box>
+            <UnorderedList
+              listStyleType="none"
+              bgColor="whitesmoke"
+              w="475px"
+              ml="0px"
+              h="44px"
+              pl="100px"
+            >
+              <ListItem>
+                <BiStoreAlt />
+                <Button colorScheme="black" variant="link">
+                  Find a Store
+                </Button>
+              </ListItem>
+            </UnorderedList>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default NavUpperMenu;
